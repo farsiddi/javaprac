96457682a5c2cf9508efdb30cb66beddddb7b50c
Siddiqui <farhan.a.siddiqui@capgemini.com>
2023-11-27 12:35:16 +0530

    main file

---
+++ Trash/Main.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+//        name is pointing to the object in the string pool
+//        String pool is the separate memory structure inside the heap
+       String name = "Farhan Siddiqui";
+        System.out.println(name);
+
+//        a and b is pointing to same object in heap
+        String a = "Karan";
+        String b = "karan";
+        b="Rohan";
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
